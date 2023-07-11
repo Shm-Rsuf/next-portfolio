f3a504f3f83785e17c9a20b4fe7ae0eabc650c71
shm_usuf <shmusuf@gmail.com>
2023-07-11 21:50:34 +0600

    sectionHeading added

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -3,7 +3,7 @@ interface SectionHeadingText {
 }
 const SectionHeading: React.FC<SectionHeadingText> = ({ text }) => {
   return (
-    <h2 className="text-4xl sm:text-6xl text-gray-50 text-center py-10">
+    <h2 className="text-4xl sm:text-6xl text-gray-50 text-center py-10 uppercase">
       {text}
     </h2>
   );
